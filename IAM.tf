22fdfd5752fc84328c3eb9b3f8c860699a4dfc0d
Prince <67404137+princeansah@users.noreply.github.com>
2020-08-24 00:41:49 -0500

    terraform files for automation

--- IAM.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_user" "lb" {
-  name = "terraformuser"
-}
-resource "aws_iam_group" "developers" {
-  name = "developers"
-}
--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "" {
-    ami = "ami-0a63f96e85105c6d3"
-    instance_type = "t2.micro"
-    key_name = "ice"
-tags = {
-    Name = "VSc_personal"
-}
-}
